382a754cf006df78b9e57110a6769b4f97af228a
Mehul Rami <mehul3446@gmail.com>
2023-08-26 19:19:49 -0400

    added winnigs function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #global constants
+import random
+
 MIN_LINES = 1
 MAX_LINES = 3
 MIN_BET = 1
@@ -6,6 +8,49 @@ MAX_BET = 100
 
 ROWS = 3
 COLS = 3
+
+symbol_count = {
+    "A" : 2,
+    "B" : 4,
+    "C" : 6,
+    "D" : 8
+}
+
+symbol_value = {
+    "A" : 5,
+    "B" : 4,
+    "C" : 3,
+    "D" : 2
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+        
+        columns.append(column)
+
+    return columns
+
+def print_slot_machines(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        print()
+
 
 
 print("Begin Program")
@@ -53,6 +98,23 @@ def get_bet():
         else:
             print("please enter a valid value")
     return bet 
+
+def check_winnnigs(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
+
+
  
 
 #Defining main
@@ -68,6 +130,11 @@ def main():
         else:
             break
     print(f"You have chosen to bet ${bet} on {lines} lines. Total bet: ${total}")
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machines(slots)
+    winnings, winning_lines = check_winnnigs(slots, lines, bet, symbol_value)
+    print(f"You won: ${winnings}")
+    print(f"You won on lines:", *winning_lines)
 
 #Calling main
 
